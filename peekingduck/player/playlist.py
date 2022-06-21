c3d7ecfd61d39f2552240336c7f4057c4dcb8542
dotw <davidong@aisingapore.org>
2022-06-21 17:08:55 +0800

    - add PipelineStats class to encapsulate pipeline file information - refactor playlist.py to work with PipelineStats

--- peekingduck/player/playlist.py
+++ peekingduck/player/playlist.py
@@ -24,6 +24,36 @@ import yaml
 # Globals
 PKD_CONFIG_DIR = ".peekingduck"
 PKD_PLAYLIST_FILE = "playlist.yaml"
+PKD_PLAYLIST_HEADER = ["pipeline", "status", "datetime"]
+
+
+class PipelineStats:
+    """Implements the Pipeline Stats class to store pipeline-related information."""
+
+    def __init__(self, pipeline: Path, exist: bool, mod_datetime: float) -> None:
+        self._pipeline = pipeline
+        self._exist = exist
+        self._mod_datetime = mod_datetime
+        self._headers = PKD_PLAYLIST_HEADER
+
+    def __str__(self) -> str:
+        return self._pipeline.name
+
+    @property
+    def exist(self) -> bool:
+        return self._exist
+
+    @property
+    def mod_datetime(self) -> str:
+        return datetime.fromtimestamp(self._mod_datetime).strftime("%Y-%m-%d-%H:%M:%S")
+
+    @property
+    def pipeline(self) -> str:
+        return str(self._pipeline)
+
+    @property
+    def headers(self) -> List[str]:
+        return self._headers
 
 
 class PlayList:
@@ -35,6 +65,7 @@ class PlayList:
     - remove pipeline from playlist     O(N)
     - access playlist[i]                O(1)
     - update playlist[i]                O(1)
+    - check pipeline in playlist        O(1)
     - load and save
     """
 
@@ -55,6 +86,8 @@ class PlayList:
         self._iter_idx += 1
         if self._iter_idx < len(self._pipeline_list):
             return self._pipeline_list[self._iter_idx]
+            # pipeline = self._pipeline_list[self._iter_idx]
+            # return self._pipeline_stats[pipeline]
         raise StopIteration
 
     def __getitem__(self, i: int) -> str:
@@ -62,6 +95,7 @@ class PlayList:
 
         if 0 <= i < len(pipeline_list):
             return pipeline_list[i]
+            # return self._pipeline_stats[pipeline_list[i]]
 
         k = len(pipeline_list)
         raise ValueError(
@@ -73,7 +107,9 @@ class PlayList:
         pipeline_list = self._pipeline_list
 
         if 0 <= i < len(pipeline_list):
-            pipeline_list[i] = value
+            self._pipeline_stats.pop(pipeline_list[i])  # remove old item
+            pipeline_list[i] = value  # add and update new item
+            self._pipeline_stats[value] = self._make_pipeline_stats(value)
         else:
             k: int = len(pipeline_list)
             raise ValueError(
@@ -82,7 +118,7 @@ class PlayList:
             )
 
     def __contains__(self, item):
-        res = str(item) in self._pipeline_list
+        res = str(item) in self._pipeline_stats
         # print(f"contains: item={type(item)} {item}, res={res}")
         return res
 
@@ -90,13 +126,21 @@ class PlayList:
         res = "\n".join([f"{i} -> {k}" for i, k in enumerate(self._pipeline_list)])
         return res
 
+    def get_stats(self, pipeline: str) -> PipelineStats:
+        return self._pipeline_stats[pipeline]
+
     def add_pipeline(self, pipeline_path: Union[Path, str]) -> None:
         """Add pipeline yaml file to playlist
 
         Args:
             pipeline_path (Union[Path, str]): path of yaml file to add
         """
-        self._pipeline_list.append(str(pipeline_path))
+        pipeline_str = str(pipeline_path)
+        if pipeline_path in self:
+            raise ValueError(f"Error adding existing {pipeline_path}")
+        # add new pipeline and stats
+        self._pipeline_list.append(pipeline_str)
+        self._pipeline_stats[pipeline_str] = self._make_pipeline_stats(pipeline_str)
 
     def remove_pipeline(self, pipeline_path: Union[Path, str]) -> None:
         """Remove pipeline yaml file from playlist
@@ -104,7 +148,12 @@ class PlayList:
         Args:
             pipeline_path (Union[Path, str]): path of yaml file to remove
         """
-        self._pipeline_list.remove(str(pipeline_path))
+        pipeline_str = str(pipeline_path)
+        if pipeline_path not in self:
+            raise ValueError(f"Error removing non-existent {pipeline_path}")
+        # remove old pipeline and stats
+        self._pipeline_list.remove(pipeline_str)
+        self._pipeline_stats.pop(pipeline_str)
 
     def load_playlist_file(self) -> None:
         """Load playlist file"""
@@ -136,22 +185,27 @@ class PlayList:
     def _verify_pipeline_files(self) -> None:
         self.logger.info(f"pipelines={self._pipeline_list}")
         self._pipeline_stats = dict.fromkeys(self._pipeline_list, None)
+        assert len(self._pipeline_stats.keys()) == len(self._pipeline_list)
 
         for pipeline in self._pipeline_list:
-            is_exist = False  # file exist flag
-            mod_datetime = None  # last modified date/time
-
-            pipeline_path_obj = Path(pipeline)
-            if pipeline_path_obj.exists():
-                is_exist = True
-                mod_time = pipeline_path_obj.stat().st_mtime
-                mod_datetime = datetime.fromtimestamp(mod_time).strftime(
-                    "%Y-%m-%d-%H:%M:%S"
-                )
-
-            self._pipeline_stats[pipeline] = {
-                "exist": is_exist,
-                "mtime": mod_datetime,
-            }
+            self._pipeline_stats[pipeline] = self._make_pipeline_stats(pipeline)
 
         self.logger.info(f"stats={self._pipeline_stats}")
+
+    def _make_pipeline_stats(self, pipeline: str) -> PipelineStats:
+        pipeline_path = Path(pipeline)
+        file_exist = False  # file exist flag
+        mod_datetime = None  # last modified date/time
+        if pipeline_path.exists():
+            file_exist = True
+            mod_datetime = pipeline_path.stat().st_mtime
+            # mod_datetime = datetime.fromtimestamp(mod_time).strftime(
+            #     "%Y-%m-%d-%H:%M:%S"
+            # )
+
+        # self._pipeline_stats[pipeline] = {
+        #     "exist": file_exist,
+        #     "mtime": mod_datetime,
+        # }
+
+        return PipelineStats(pipeline_path, file_exist, mod_datetime)
